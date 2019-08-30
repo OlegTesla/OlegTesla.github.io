890ad24ee3f862ae1584bde71182424b8a8bed81
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2019-08-30 15:35:03 +0300

    Add files via upload

--- js/slaider.js
+++ js/slaider.js
@@ -2,14 +2,11 @@ $(document).ready(function(){
   $('.head__slaider__slide, .clients__slaider__slide, .reviews__slide').slick({
   	 dots: true,
 	  arrows: true
-  });
-    $(".navmob__down").click(function(){
-  	$(this).next(".navmob__submenu").slideToggle();
-  });
-    $('.navmob__menu').hide();
-    $('.hamb__container').click(function(){
+  });	
+    $('.navmob__menu__hide').hide();
+    $('.navmob__container__left').click(function(){
     	$(this).children('.hamb__active');
-    	$('.navmob__menu').slideToggle();
+    	$('.navmob__menu__hide').slideToggle();
     	$('.hamb').toggleClass("hamb__active");
     });
 });
