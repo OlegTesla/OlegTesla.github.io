40cd487720b09a05760e1061f5acda3ec9216ac0
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2019-09-23 18:06:47 +0300

    Add files via upload

--- cooking/js/script.js
+++ cooking/js/script.js
@@ -3,9 +3,16 @@ $(function() {
 	});
 });
 
-
 $(function() {
 	$('.burger,.navmob__menu').on('click',function() {
 		$('.header__navmob__list').slideToggle('.header__item');
 	});
+});
+
+$(document).ready(function() {
+$(".up__btn").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".header").offset().top
+     }, 1500);
+ });
 });
