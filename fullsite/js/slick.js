fb78517ca3e7ba095d0333ccd1500f1cf97a8dcd
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2019-10-17 23:01:17 +0300

    Add files via upload

---
+++ fullsite/js/slick.js
@@ -0,0 +1,26 @@
+// first slaider
+$('.header__slaider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: true,
+	arrows: false
+});
+
+// second slaider
+$('.best__container').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  arrows: true
+});
+
+// third slaider
+$('.clients__sliader').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	dots: true,
+	arrows: false
+});
