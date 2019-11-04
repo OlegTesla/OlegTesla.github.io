98de7d39ba247802809be09d3b405f2147493c91
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2019-11-04 21:23:30 +0300

    Add files via upload

---
+++ fullsite/js/typed-text.js
@@ -0,0 +1,12 @@
+$('document').ready(function() {
+	var typed = new Typed('#type',{
+		strings: [
+			"make a good design?",
+			"make a lovely maket?",
+			"make a nice day?"
+		],
+		typeSpeed: 50,
+		backSpeed: 50,
+		loop: true	
+	});
+});
