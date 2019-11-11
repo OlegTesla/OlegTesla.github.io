d6acb68d92dced9137484bdb6769f1ad941cf4b9
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2019-11-11 05:19:33 +0300

    Add files via upload

--- fullsite/js/script.js
+++ fullsite/js/script.js
@@ -19,9 +19,37 @@ $(document).ready(function() {
 	$(".header__navmob").removeClass("default");
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 20) {
-			$(".header__navmob").addClass("default").fadeIn("2000");
+			$(".header__navmob").addClass("default");
 		} else {
-			$(".header__navmob").removeClass("default").fadeIn("slow");
+			$(".header__navmob").removeClass("default");
 		};
 	});
 });
+/*
+// scroll to top
+$("a[href='#scroll']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+*/
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  $('html,body').animate({ scrollTop: 0 }, 'slow');
+  document.documentElement.scrollTop = 0;
+}
