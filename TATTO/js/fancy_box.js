1fc5d1f0f390e87005e5b08eaa0f3e43c8c29e09
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2020-01-07 21:42:28 +0200

    Add files via upload

---
+++ TATTO/js/fancy_box.js
@@ -0,0 +1,13 @@
+$('[data-fancybox="gallery"]').fancybox({
+  loop: true,
+  buttons: [
+    "zoom",
+    "share",
+    "slideShow",
+    "fullScreen",
+    "download",
+    "thumbs",
+    "close"
+  ],
+  animationEffect: "zoom-in-out"
+});
