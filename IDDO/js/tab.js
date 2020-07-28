96397e5fc1ba23da67d76bef3a7533b82061f5b5
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2020-07-28 11:44:46 +0200

    Add files via upload

---
+++ IDDO/js/tab.js
@@ -0,0 +1,42 @@
+$(function() {
+	let filter = $("[data-filter]");
+	filter.on("click", function(event){
+		event.preventDefault();
+		let cat = $(this).data('filter');
+		if(cat == "all") {
+			$("[data-cat]").removeClass('hide');
+		} else {
+			$("[data-cat]").each(function(){
+				let workCat = $(this).data('cat');
+				if(workCat != cat) {
+					$(this).addClass('hide');
+				} else {
+					$(this).removeClass('hide');
+				}
+			});
+		}
+	});
+});
+
+// slaider
+$(document).ready(function(){
+  $('.slaider__box').slick({
+  	arrows: false,
+  	dots: true
+  });
+});
+// progress bar 
+ function Circlle(el) {
+ 	$(el).circleProgress({fill: {color: "#ca3c3c"}})
+ 	.on("circle-animation-progress" , function(event, progress, stepValue) {
+ 		$(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+"%");
+ 	});
+ };
+ Circlle(".round");
+// mobile menu
+$(document).ready(function(){
+ $('.header__burger').click(function(event){
+ 	$('.header__burger,.nav__navigation').toggleClass('active');
+ });
+});
+
