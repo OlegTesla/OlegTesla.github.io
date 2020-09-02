2c232dce8287f739370d42691bd9e864b6fc3dd9
OlegTesla <54645721+OlegTesla@users.noreply.github.com>
2020-09-02 13:40:19 +0200

    Add files via upload

--- MUSLINSOPT/js/script.js
+++ MUSLINSOPT/js/script.js
@@ -11,3 +11,63 @@ $(document).ready(function(){
 		$(this).next('.accordion-item__content').slideToggle(200);
 	});
 });
+// filter 
+$(function() {
+	let filter = $("[data-filter]");
+	filter.on("click", function(event){
+		event.preventDefault();
+		let cat = $(this).data('filter');
+		if(cat == "all") {
+			$("[data-cat]").removeClass('hide');
+		} else {
+			$("[data-cat]").each(function(){
+				let workCat = $(this).data('cat');
+				if(workCat != cat) {
+					$(this).addClass('hide');
+				} else {
+					$(this).removeClass('hide');
+				}
+			});
+		}
+	});
+	$('.filter__nav-link').click(function(event){
+		$(".filter__nav-link").toggleClass('active');
+	});
+});
+
+let tab = function () {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('is-active');
+        });
+        this.classList.add('is-active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
+
+};
+
+tab();
+
+// arrow on acordion function
+$(document).ready(function(){
+	$('.accordion-item__triger').click(function(event){
+		$(".arrow").toggleClass('active');
+	})
+});
+
+
